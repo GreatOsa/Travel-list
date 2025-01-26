632d932125d2401ab3f2b89750fb9cfc05b0350f
Agwaze Great <agwazegreat@gmail.com>
2025-01-26 02:14:14 +0100

    still the same, just change the folder and deleted some file

--- src/App.js
+++ src/App.js
@@ -1,44 +1,10 @@
-import React, { useState } from "react";
-import Logo from "./component/Logo";
-import Form from "./component/Form";
-import PackingList from "./component/PackingList";
-import Stats from "./component/Stats";
+import React from "react";
+import DateForm from "./component/DateForm";
 
 export default function App() {
-  const [items, setItems] = useState([]);
-  function handleAddItem(item) {
-    setItems((items) => [...items, item]);
-  }
-
-  function handleDeleteItem(id) {
-    setItems((items) => items.filter((items) => items.id !== id));
-  }
-
-  function handleToggleItems(id) {
-    setItems((items) =>
-      items.map((item) =>
-        item.id === id ? { ...item, packed: !item.packed } : item
-      )
-    );
-  }
-  function handleClearList() {
-    const confirmed = window.confirm(
-      "Are you sure you want to delete all items?"
-    );
-    if (confirmed) setItems([]);
-  }
-
   return (
     <div className="app">
-      <Logo />
-      <Form onAddItem={handleAddItem} />
-      <PackingList
-        items={items}
-        onToggleItem={handleToggleItems}
-        onDeleteItem={handleDeleteItem}
-        onClearList={handleClearList}
-      />
-      <Stats items={items} />
+      <div className=" container">{/* <DateForm /> */}</div>
     </div>
   );
 }
---
+++ src/component/DateForm.js
@@ -0,0 +1,22 @@
+import React from "react";
+
+export default function DateForm() {
+  return (
+    <div>
+      <form>
+        <label>
+          <span>Days</span>
+          <input type="text" />
+        </label>
+        <label>
+          <span>Days</span>
+          <input type="text" />
+        </label>
+        <label>
+          <span>Days</span>
+          <input type="text" />
+        </label>
+      </form>
+    </div>
+  );
+}
